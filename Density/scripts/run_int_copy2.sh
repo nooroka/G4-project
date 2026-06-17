3e3bed31bd2f5d693bdad2e097752798f78c5d4b
Vera Panova <nooroka@mouse.belozersky.msu.ru>
2026-06-17 19:00:31 +0300

    updates5

--- Density/scripts/run_int_copy2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for threshold in 39 40; do
- #   for control in control2_cleaned; do
-        echo "Running: threshold=${threshold}"
-       # python intervals_110426_v8.py ${threshold} ${control}
-        python intervals_110426_v8_copy2.py ${threshold} 
-   # done
-done
